5e2f76ecd497342217ff520bb670c3ee022e039f
root <jmedina@streamingmediahosting.com>
2020-04-01 23:00:04 +0000

    latest update

--- web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
+++ web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
@@ -1,11 +1,14 @@
-kWidget.addReadyCallback(function (playerId) {
-    var kdp = document.getElementById(playerId);
-    console.log(kdp);
-    kdp.kBind("playerReady", function () {
-        if (kdp.evaluate('{mediaProxy.entry.type}') === 1) {
-
+var kdp = null;
+kWidget.readyCallback(function (playerId) {
+        var kdp = $('#' + playerId );
+        console.log(kdp);
+        kdp.kBind("playerReady", function () {
             console.log('SMH TEEST');
+            if (kdp.evaluate('{mediaProxy.entry.type}') === 1) {
 
-        }
-    });
+                console.log('SMH TEEST');
+
+            }
+        });
+
 });
