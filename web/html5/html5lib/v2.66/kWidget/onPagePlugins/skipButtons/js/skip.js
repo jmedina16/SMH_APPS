26a6d3a34c4e078fa1ed2f407dffdaaf9ea460f2
root <jmedina@streamingmediahosting.com>
2020-04-03 21:00:03 +0000

    latest update

--- web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
+++ web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
@@ -1,14 +1,26 @@
 var kdp = null;
-kWidget.readyCallback(function (playerId) {
-        var kdp = $('#' + playerId );
-        console.log(kdp);
+console.log('TEEEST');
+kWidget.addReadyCallback(function (playerId) {
+    console.log('READY CALL BACK FIRED');
+    var kdp = document.getElementById(playerId);
+    var playerType = kdp.nodeName.toLowerCase();
+    if (playerType == "object") {
+        kdp.kBind("entryReady", function () {
+           console.log('SMH TEEST');
+        });
+    } else {
         kdp.kBind("playerReady", function () {
             console.log('SMH TEEST');
-            if (kdp.evaluate('{mediaProxy.entry.type}') === 1) {
-
-                console.log('SMH TEEST');
-
-            }
         });
+    }
+//    console.log(kdp);
+//    kdp.kBind("playerReady", function () {
+//        console.log('SMH TEEST');
+//        if (kdp.evaluate('{mediaProxy.entry.type}') === 1) {
+//
+//            console.log('SMH TEEST');
+//
+//        }
+//    });
 
 });
