750b11bb31215fab46725f76c8c1104f262d5c7e
root <jmedina@streamingmediahosting.com>
2020-04-03 22:00:04 +0000

    latest update

--- web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
+++ web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipButtons/js/skip.js
@@ -1,26 +1,71 @@
-var kdp = null;
-console.log('TEEEST');
-kWidget.addReadyCallback(function (playerId) {
-    console.log('READY CALL BACK FIRED');
-    var kdp = document.getElementById(playerId);
-    var playerType = kdp.nodeName.toLowerCase();
-    if (playerType == "object") {
-        kdp.kBind("entryReady", function () {
-           console.log('SMH TEEST');
-        });
-    } else {
-        kdp.kBind("playerReady", function () {
-            console.log('SMH TEEST');
-        });
-    }
-//    console.log(kdp);
-//    kdp.kBind("playerReady", function () {
-//        console.log('SMH TEEST');
-//        if (kdp.evaluate('{mediaProxy.entry.type}') === 1) {
-//
-//            console.log('SMH TEEST');
-//
-//        }
-//    });
-
-});
+(function (mw, $) {
+    "use strict";
+    mw.kalturaPluginWrapper(function () {
+        mw.PluginManager.add('skipButtons', mw.KBaseComponent.extend({
+            // public properties
+            defaultConfig: {
+                // the container for the button
+                parent: "controlsContainer",
+                // the display order ( based on layout )
+                order: 81,
+                // the display importance, determines when the item is removed from DOM
+                displayImportance: 'high',
+                // the alignment of the button
+                align: "right",
+                // custom property and custom value
+                seekTimeConfig: "30"
+            },
+            canSeek: false,
+            setup: function () {
+                // initialization code goes here.
+                // call a method for event bindings:
+                this.addBindings();
+            },
+            addBindings: function () {
+                var _this = this;
+                this.bind('playerReady', function () {
+                    console.log("player is ready");
+                });
+                this.bind('updateBufferPercent', function () {
+                    _this.canSeek = true;
+                });
+            }, 
+            getComponent: function () {
+                var _this = this;
+                if (!this.$el) {
+                    this.$el = $('<button />')
+                            .attr('title', 'Click Me!')
+                            .addClass('btn icon-myicon' + this.getCssClass())
+                            .click(function () {
+                                _this.fireMyCustomEvent();
+                            });
+                }
+                return this.$el;
+            },
+            fireMyCustomEvent: function () {
+                // dispatch the 'myCustomEventName' event
+                console.log("IT WORKED!!");
+            },
+            seek: function (direction) {
+                if (!this.canSeek) {
+                    return false;
+                }
+                var seekTime = parseFloat(this.getConfig('seekTimeConfig'));
+                var currentTime = parseFloat(this.getPlayer().currentTime);
+                var newCurrentTime = 0;
+                if (direction == 'back') {
+                    newCurrentTime = currentTime - seekTime;
+                    if (newCurrentTime < 0) {
+                        newCurrentTime = 0;
+                    }
+                } else {
+                    newCurrentTime = currentTime + seekTime;
+                    if (newCurrentTime > parseFloat(this.getPlayer().getDuration())) {
+                        newCurrentTime = parseFloat(this.getPlayer().getDuration());
+                    }
+                }
+                this.getPlayer().seek(newCurrentTime);
+            },
+        }));
+    });
+})(window.mw, window.jQuery);
