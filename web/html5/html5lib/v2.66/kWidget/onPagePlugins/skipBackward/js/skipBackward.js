cf8c8e70c5a83c9f4e92ff36810b7af8375c2ac9
root <jmedina@streamingmediahosting.com>
2020-04-04 00:00:06 +0000

    latest update

---
+++ web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipBackward/js/skipBackward.js
@@ -0,0 +1,58 @@
+(function (mw, $) {
+    "use strict";
+    mw.kalturaPluginWrapper(function () {
+        mw.PluginManager.add('skipBackward', mw.KBaseComponent.extend({
+            // public properties
+            defaultConfig: {
+                // the container for the button
+                parent: "controlsContainer",
+                // the display order ( based on layout )
+                order: 81,
+                // the display importance, determines when the item is removed from DOM
+                displayImportance: 'high',
+                // the alignment of the button
+                align: "right",
+                // custom property and custom value
+                seekTimeConfig: "30"
+            },
+            canSeek: false,
+            setup: function () {
+                // initialization code goes here.
+                // call a method for event bindings:
+                this.addBindings();
+            },
+            addBindings: function () {
+                var _this = this;
+                this.bind('updateBufferPercent', function () {
+                    _this.canSeek = true;
+                });
+            },
+            getComponent: function () {
+                var _this = this;
+                if (!this.$el) {
+                    this.$el = $('<button />')
+                            .attr('data-show-tooltip', true)
+                            .attr('title', 'Skip Backward')
+                            .addClass('btn icon-prev' + this.getCssClass())
+                            .click(function () {
+                                _this.seek();
+                            });
+                }
+                return this.$el;
+            },
+            seek: function () {
+                if (!this.canSeek) {
+                    return false;
+                }
+                var seekTime = parseFloat(this.getConfig('seekTimeConfig'));
+                var currentTime = parseFloat(this.getPlayer().currentTime);
+                var newCurrentTime = 0;
+                newCurrentTime = currentTime - seekTime;
+                if (newCurrentTime < 0) {
+                    newCurrentTime = 0;
+                }
+                this.getPlayer().seek(newCurrentTime);
+            },
+        }));
+    });
+})(window.mw, window.jQuery);
---
+++ web/html5/html5lib/v2.66/kWidget/onPagePlugins/skipForward/js/skipForward.js
@@ -0,0 +1,57 @@
+(function (mw, $) {
+    "use strict";
+    mw.kalturaPluginWrapper(function () {
+        mw.PluginManager.add('skipForward', mw.KBaseComponent.extend({
+            // public properties
+            defaultConfig: {
+                // the container for the button
+                parent: "controlsContainer",
+                // the display order ( based on layout )
+                order: 71,
+                // the display importance, determines when the item is removed from DOM
+                displayImportance: 'high',
+                // the alignment of the button
+                align: "right",
+                // custom property and custom value
+                seekTimeConfig: "30"
+            },
+            canSeek: false,
+            setup: function () {
+                // initialization code goes here.
+                // call a method for event bindings:
+                this.addBindings();
+            },
+            addBindings: function () {
+                var _this = this;
+                this.bind('updateBufferPercent', function () {
+                    _this.canSeek = true;
+                });
+            },
+            getComponent: function () {
+                var _this = this;
+                if (!this.$el) {
+                    this.$el = $('<button />')
+                            .attr('title', 'Skip Forward')
+                            .addClass('btn icon-next' + this.getCssClass())
+                            .click(function () {
+                                _this.seek();
+                            });
+                }
+                return this.$el;
+            },
+            seek: function () {
+                if (!this.canSeek) {
+                    return false;
+                }
+                var seekTime = parseFloat(this.getConfig('seekTimeConfig'));
+                var currentTime = parseFloat(this.getPlayer().currentTime);
+                var newCurrentTime = 0;
+                newCurrentTime = currentTime + seekTime;
+                if (newCurrentTime > parseFloat(this.getPlayer().getDuration())) {
+                    newCurrentTime = parseFloat(this.getPlayer().getDuration());
+                }
+                this.getPlayer().seek(newCurrentTime);
+            },
+        }));
+    });
+})(window.mw, window.jQuery);
